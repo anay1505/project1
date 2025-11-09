d4b8d5d855d27a1a987823a954533aa6bb143c69
Anay <anay1505@gmail.com>
2025-11-09 23:40:03 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello") //we are using nodejs not browser
+
+console.log("hello")
+
+let name = "anay"
+let age = 21
+let isLoggedIn = false
+let state; //undefined
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => unique
+//object => key value pairs
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
