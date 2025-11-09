891ca195510b810bd8a73f7941a2b68cbb8408ca
Anay <anay1505@gmail.com>
2025-11-09 23:41:35 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID=14453
+let accountEmail = "anaygoogle@gmail.com"
+var accountPassword = "696969"
+accountCity ="Mirzapur"
+let accountState;
+
+//accountID = 2 //not  allowed
+accountEmail= "hchc@gmail.com"
+accountPassword="jdhf"
+accountCity="Rath"
+
+console.log(accountID)
+/*prefer not to use var
+because of issue in block scope and functional scope */
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
